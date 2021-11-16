befa1b18bbda614b9e64873803be477110e1c947
visa-bulletin <visabulletin2020@gmail.com>
2021-11-16 06:20:10 -0500

    Dec 2021

--- js/daterange.js
+++ js/daterange.js
@@ -1,5 +1,5 @@
 // Set the startDateStr and endDateStr according to the data
 var startDateStr = "January 2015";
-var endDateStr = "November 2021";
+var endDateStr = "December 2021";
 // console.log(startDateStr);
 // console.log(endDateStr);
